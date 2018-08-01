f7ed90949f43138149b6df718bb1f16c3434281c
OscarJavierHernandez <javierhernandezmelgar@gmail.com>
2018-08-01 15:40:35 +0200

    added all code to the server file

--- server_code/create_tables_and_listen.sh
+++ server_code/create_tables_and_listen.sh
@@ -2,16 +2,16 @@
 
 # Create the location based data base and run the script
 echo "creating location based database"
-sqlite3 location_based_database.db << EOF
+sqlite3 Databases/location_based_database.db << EOF
 .read create_databases.sql
 EOF
 
 echo "creating keyword based database"
-sqlite3 keyword_based_database.db << EOF
+sqlite3 Databases/keyword_based_database.db << EOF
 .read create_databases.sql
 EOF
 
 echo "beginning python scripts"
-python location_listener.py &
-python keyword_listener.py &
+#python location_listener.py &
+#python keyword_listener.py &
 
